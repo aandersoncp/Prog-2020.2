343c7e4153ff71e8a7a75ca5df798c2f060f5d26
Anderson Costa <38294778+aandersoncp@users.noreply.github.com>
2021-03-05 13:21:57 -0300

    Update main.c

--- lab10/main.c
+++ lab10/main.c
@@ -1,35 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "intlst.h"
 
 int main(int narg, char *argv[]) {
 	Lista *l;
+
+	char nome[MAXNOME], email[MAXNOME], tipo;
+	int matr, ddd, tel;
 	
 	l = lst_inicializa();
 	
-	if (lst_inserir(&l, 01, "josé", 85, 87878787, "jose@mail.com", 'c')){
-		printf("Inserido com sucesso.\n");
-	}
-	else{
-		printf("Falha ao inserir.\n");
-	}
-
-	if (lst_inserir(&l, 02, "maria", 88, 32868686, "maria@mail.com", 'f'))
-		printf("Inserido com sucesso.\n");
-	else
-		printf("Falha ao inserir.\n");
-
-	if (lst_inserir(&l, 03, "francisco", 88, 32328686, "frank@mail.com", 'f'))
-		printf("Inserido com sucesso.\n");
-	else
-		printf("Falha ao inserir.\n");
-
-	percorrer(&l);
-	printf("\n");
-	carregar(&l, argv[1]);
-	salvar(&l, "teste");
-	percorrer(&l);
+	menu(&l, argv[1]);
 	
 	return EXIT_SUCCESS;
 }
-
